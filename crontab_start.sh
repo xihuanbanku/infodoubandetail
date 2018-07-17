6175d0806a5f0287376a800f4b70961a8a1cc2f2
xihuanbanku <lianghaikun@sina.com>
2018-07-17 17:10:40 +0800

    使用绝对路径nohup写文件

--- crontab_start.sh
+++ crontab_start.sh
@@ -6,24 +6,24 @@ html=`curl -L movie.douban.com`
 error_msg="检测到有异常请求"
 if [[ ${html} =~ ${error_msg} ]]
 then
-    echo `date`"[IP被限制]" >> nohup.out
-    echo `date`"[停止爬虫]" >> nohup.out
+    echo `date`"[IP被限制]" >> /home/hadoop/deploy/spider/infodoubandetail/nohup.out
+    echo `date`"[停止爬虫]" >> /home/hadoop/deploy/spider/infodoubandetail/nohup.out
     pkill -ef "douban_info"
     echo
     sleep 3
-    echo `date`"[停止完成]" >> nohup.out
+    echo `date`"[停止完成]" >> /home/hadoop/deploy/spider/infodoubandetail/nohup.out
 else
-    echo `date`"[IP解除限制, 检查是否已经启动过爬虫...]" >> nohup.out
+    echo `date`"[IP解除限制, 检查是否已经启动过爬虫...]" >> /home/hadoop/deploy/spider/infodoubandetail/nohup.out
     pid=`pgrep -f "douban_info/run"`
     sleep 3
     if [ ${pid} > 0 ]
     then
-        echo `date`"[爬虫已经启动pid[${pid}], 直接退出...]" >> nohup.out
+        echo `date`"[爬虫已经启动pid[${pid}], 直接退出...]" >> /home/hadoop/deploy/spider/infodoubandetail/nohup.out
     else
-        echo `date`"[爬虫没有启动, starting...]" >> nohup.out
+        echo `date`"[爬虫没有启动, starting...]" >> /home/hadoop/deploy/spider/infodoubandetail/nohup.out
         sh start.sh
         sleep 3
-        echo `date`"[启动成功]" >> nohup.out
+        echo `date`"[启动成功]" >> /home/hadoop/deploy/spider/infodoubandetail/nohup.out
     fi
 
 fi
