db0ef1ff7be4cf81123073fc6fd7a76a05c1f4b1
xihuanbanku <lianghaikun@sina.com>
2018-07-30 16:28:21 +0800

    增加日志提示信息, 与其他日志进行区别

--- crontab_start.sh
+++ crontab_start.sh
@@ -5,25 +5,25 @@ cd /home/hadoop/deploy/spider/infodoubandetail
 html=`curl -L movie.douban.com`
 error_msg="检测到有异常请求"
 if [[ ${html} =~ ${error_msg} ]]
-then
-    echo `date`"[IP被限制]" >> nohup.out
-    echo `date`"[停止爬虫]" >> nohup.out
-    pkill -ef "douban_info"
-    echo
-    sleep 3
-    echo `date`"[停止完成]" >> nohup.out
-else
-    echo `date`"[IP解除限制, 检查是否已经启动过爬虫...]" >> nohup.out
-    pid=`pgrep -f "douban_info/run"`
-    sleep 3
-    if [ ${pid} > 0 ]
-    then
-        echo `date`"[爬虫已经启动pid[${pid}], 直接退出...]" >> nohup.out
-    else
-        echo `date`"[爬虫没有启动, starting...]" >> nohup.out
-        sh start.sh
-        sleep 3
-        echo `date`"[启动成功]" >> nohup.out
-    fi
+	then
+		echo `date`"[infodoubandetailIP被限制]" >> nohup.out
+		echo `date`"[infodoubandetail停止爬虫]" >> nohup.out
+		pkill -ef "douban_info"
+		echo
+		sleep 3
+		echo `date`"[infodoubandetail停止完成]" >> nohup.out
+	else
+		echo `date`"[infodoubandetail][IP解除限制, 检查是否已经启动过爬虫...]" >> nohup.out
+		pid=`pgrep -f "douban_info/run"`
+		sleep 3
+		if [ ${pid} > 0 ]
+		then
+			echo `date`"[infodoubandetail爬虫已经启动pid[${pid}], 直接退出...]" >> nohup.out
+		else
+			echo `date`"[infodoubandetail爬虫没有启动, starting...]" >> nohup.out
+			sh start.sh
+			sleep 3
+			echo `date`"[infodoubandetail启动成功]" >> nohup.out
+		fi
 
 fi
